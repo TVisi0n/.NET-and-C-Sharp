39ce933fa9a0a46109cb4050bbb5b6425a73a7f9
Tanner Vasquez <126614745+TVisi0n@users.noreply.github.com>
2023-11-23 11:06:01 -0800

    Created a daily report

---
+++ DailyReport/DailyReport/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            string coureName = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Do you need help with anything? Please answer “true” or “false.”");
+            bool help = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string experiences = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            int studyTime = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+        }
+    }
+}
