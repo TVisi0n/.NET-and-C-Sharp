8720962cc1027b4054378d17efb9b66e2d77f8b6
Tanner Vasquez <126614745+TVisi0n@users.noreply.github.com>
2023-11-25 18:06:04 -0800

    Created an insurance qualification app

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gecko Car Insurance Approval Form");
+
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? Please enter \"true\" or \"false\"");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qualified?");
+            bool qualOrDeny = age > 15 && dui == false && tickets <= 3;
+            Console.WriteLine(qualOrDeny);
+
+            Console.ReadLine();
+        }
+    }
+}
