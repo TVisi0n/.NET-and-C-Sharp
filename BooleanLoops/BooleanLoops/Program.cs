b9aec582c9a7021165145f6e154c67e75c981d02
Tanner Vasquez <126614745+TVisi0n@users.noreply.github.com>
2023-11-26 17:05:58 -0800

    Created a boolean comparison app

---
+++ BooleanLoops/BooleanLoops/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Guess the Veggie?");
+            //string veggie = Console.ReadLine();
+            //bool guessed = veggie == "broccoli";
+
+            //while (!guessed)
+            //{
+            //    switch (veggie)
+            //    {
+            //        case "tomato":
+            //            Console.WriteLine("You guessed tomato. Guess Again!");
+            //            Console.WriteLine("Guess the Veggie?");
+            //            veggie = Console.ReadLine();
+            //            break;
+            //        case "cucumber":
+            //            Console.WriteLine("You guessed cucumber. Guess Again!");
+            //            Console.WriteLine("Guess the Veggie?");
+            //            veggie = Console.ReadLine();
+            //            break;
+            //        case "green bean":
+            //            Console.WriteLine("You guessed green bean. Guess Again!");
+            //            Console.WriteLine("Guess the Veggie?");
+            //            veggie = Console.ReadLine();
+            //            break;
+            //        case "broccoli":
+            //            Console.WriteLine("You guessed broccoli and that's correct, congrats!");
+            //            guessed = true;
+            //            break;
+            //        default:
+            //            Console.WriteLine("That is incorrect. Please Guess Again!");
+            //            Console.WriteLine("Guess the Veggie?");
+            //            veggie = Console.ReadLine();
+            //            break;
+            //    }
+            //}
+            //Console.ReadLine();
+
+            Console.WriteLine("Guess the Veggie?");
+            string veggie = Console.ReadLine();
+            bool guessed = veggie == "broccoli";
+
+
+            do
+            {
+                switch (veggie)
+                {
+                    case "tomato":
+                        Console.WriteLine("You guessed tomato. Guess Again!");
+                        Console.WriteLine("Guess the Veggie?");
+                        veggie = Console.ReadLine();
+                        break;
+                    case "cucumber":
+                        Console.WriteLine("You guessed cucumber. Guess Again!");
+                        Console.WriteLine("Guess the Veggie?");
+                        veggie = Console.ReadLine();
+                        break;
+                    case "green bean":
+                        Console.WriteLine("You guessed green bean. Guess Again!");
+                        Console.WriteLine("Guess the Veggie?");
+                        veggie = Console.ReadLine();
+                        break;
+                    case "broccoli":
+                        Console.WriteLine("You guessed broccoli and that's correct, congrats!");
+                        guessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("That is incorrect. Please Guess Again!");
+                        Console.WriteLine("Guess the Veggie?");
+                        veggie = Console.ReadLine();
+                        break;
+                }
+            }
+            while (!guessed);
+
+            Console.ReadLine();
+        }
+    }
+}
