ae4be59ecdc307e6437b4f78a19af4cbb3079134
Tanner Vasquez <126614745+TVisi0n@users.noreply.github.com>
2023-11-26 16:22:36 -0800

    Created Package estimator

---
+++ Branching/Branching/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentTemp = 80;
+            int roomTemp = 70;
+
+            if (currentTemp == roomTemp)
+            {
+                Console.WriteLine("It is exactly room temperature.");
+            }
+            else if (currentTemp > roomTemp)
+            {
+                Console.WriteLine("It is warmer than room temperature.");
+            }
+            else if (roomTemp > currentTemp)
+            {
+                Console.WriteLine("Room temperature is warmer than current temperature.");
+            }
+            else
+            {
+                Console.WriteLine("It is not exactly room temperature.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ShippingQuoteApp/ShippingQuoteApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingQuoteApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Packagae Express. Please follow the instructions below.");
+
+            Console.WriteLine("Please enter the package weight in lbs.");
+            int weight = Convert.ToInt32(Console.ReadLine());
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Please enter the package width in inches.");
+            int width = Convert.ToInt32(Console.ReadLine());
+            if (width > 50)
+            {
+                Console.WriteLine("Package too wide to be shipped via Package Express. Have a good day");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Please enter the package height in inches.");
+            int height = Convert.ToInt32(Console.ReadLine());
+            if (height > 50)
+            {
+                Console.WriteLine("Package too tall to be shipped via Package Express. Have a good day");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Please enter the package length in inches.");
+            int length = Convert.ToInt32(Console.ReadLine());
+            if (length > 50)
+            {
+                Console.WriteLine("Package too long to be shipped via Package Express. Have a good day");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Your estimated total for shipping this package is:");
+            int cost = width * height * length * weight / 100;
+            Console.WriteLine("$" + cost);
+
+
+            Console.ReadLine();
+        }
+    }
+}
