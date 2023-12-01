e0f130601ded69a866c6301fa1e6ae7ec4e70618
Tanner Vasquez <126614745+TVisi0n@users.noreply.github.com>
2023-11-30 19:35:54 -0800

    Exception Handling

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Pick any number to divide my list of numbers by.\nMy list of numbers include: 8, 16, 90, 28, 53.");
+                int userChoice = Convert.ToInt32(Console.ReadLine());
+
+                List<int> numbers = new List<int>() { 8, 16, 90, 28, 53 };
+                foreach (int number in numbers)
+                {
+                    Console.WriteLine(number + " divide by " + userChoice + " is: " + number / userChoice);
+                }
+
+                Console.ReadLine();
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("You cannot divide the list of numbers by 0, please choose another whole number.");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please use a whole number.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
