5a40c74a951dbb95d5d1266f5f196e13a1df4da6
Tanner Vasquez <126614745+TVisi0n@users.noreply.github.com>
2023-12-03 16:49:36 -0800

    Created a calling methods app

---
+++ CallingMethods/CallingMethods/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class Class1
+    {
+        public static int MathOp1(int x)
+        {
+            return x + 5;
+        }
+        public static int MathOp2(int x)
+        {
+            return x * 2;
+        }
+        public static int MathOp3(int x)
+        {
+            return x - 3;
+        }
+
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Please choose to number to have 3 math operations done to them between 1 & 10.");
+            int userChoice = Convert.ToInt32(Console.ReadLine());
+
+            int output1 = Class1.MathOp1(userChoice);
+            int output2 = Class1.MathOp2(userChoice);
+            int output3 = Class1.MathOp3(userChoice);
+
+            Console.WriteLine(userChoice + " plus 5 equals: " + output1);
+            Console.WriteLine(userChoice + " times 2 equals: " + output2);
+            Console.WriteLine(userChoice + " minus 3 equals: " + output3);
+
+            Console.ReadLine();
+
+            
+        }
+    }
+}
