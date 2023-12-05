0bb8604b7ebb528d8515dc22e8c8bf5721417b15
Tanner Vasquez <126614745+TVisi0n@users.noreply.github.com>
2023-12-04 16:27:16 -0800

    Created a optional method app

---
+++ OptionalParameters/OptionalParameters/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters
+{
+    class Class1
+    {
+        public static int Method(int x, int y = 1)
+        {
+            return x * 3 + y;
+        }
+    }
+}
---
+++ OptionalParameters/OptionalParameters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            int usersNumber1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("If you'd like to enter anoter number you can, it's optional.");
+            int usersNumber2 = Convert.ToInt32(Console.ReadLine());
+
+            int usersAnswer = Class1.Method(usersNumber1, usersNumber2);
+            Console.WriteLine(usersAnswer);
+
+            Console.ReadLine();
+        }
+    }
+}
