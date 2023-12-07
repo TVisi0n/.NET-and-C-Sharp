a52329ff29a7841c0d12a777b13f7dd5bbdfb0c3
Tanner Vasquez <126614745+TVisi0n@users.noreply.github.com>
2023-12-06 16:42:07 -0800

    Class submission assignment

---
+++ ClassSubmission/ClassSubmission/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            divide div = new divide();
+
+            Console.WriteLine("Please enter a number to be divided by 2.");
+            int usersNumber = Convert.ToInt32(Console.ReadLine());
+
+            div.MethodDiv(usersNumber, out int result);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassSubmission/ClassSubmission/divide.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    class divide
+    {
+        public void MethodDiv(int x, out int result)
+        {
+            result = x / 2;
+        }
+    }
+}
