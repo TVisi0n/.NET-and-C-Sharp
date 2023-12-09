aa087f16e8b69dad2776c105299ceeecfc862df6
Tanner Vasquez <126614745+TVisi0n@users.noreply.github.com>
2023-12-09 10:40:48 -0800

    Practiced abstract classes

---
+++ AbstractClasses/AbstractClasses/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    public class Employee : Person
+    {
+        public override void sayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+    }  
+}
---
+++ AbstractClasses/AbstractClasses/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            employee.firstName = "Tanner";
+            employee.lastName = "Vasquez";
+
+            employee.sayName();
+            Console.ReadLine();
+        }
+    }
+}
