e363829fe93aee7b99b6d368917a96fcef8f71e2
Tanner Vasquez <126614745+TVisi0n@users.noreply.github.com>
2023-12-09 13:35:45 -0800

    Parsing enums practice

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the current day of the week.");
+                string userInput = Console.ReadLine();
+
+                DaysOfTheWeek day = DaysOfTheWeek.Saturday;
+
+
+                DaysOfTheWeek result;
+                if (Enum.TryParse(userInput, out result) && result == day)
+                {
+                    Console.WriteLine("Yes that is the correct day");
+                }
+                else
+                {
+                    Console.WriteLine("No that is not the correct day");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+
+            Console.ReadLine();
+        }
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        };
+    }
+}
