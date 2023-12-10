9fea65699a5d77bde576d3a4feb97badf9af72b4
Tanner Vasquez <126614745+TVisi0n@users.noreply.github.com>
2023-12-10 12:00:30 -0800

    Lambda Expression Practice

---
+++ LambdaExpressions/LambdaExpressions/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpressions
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
---
+++ LambdaExpressions/LambdaExpressions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>() { "Joe", "Tanner", "Nayeli", "Erick", "Chay", "Connor", "Joe", "Yoshi", "Hunter", "Han" };
+            List<int> IDs = new List<int>() { 1, 3, 65, 14, 5, 4, 7, 10, 45, 2 };
+            List<string> joeNames = new List<string>();
+
+            foreach(string name in names)
+            {
+                if (name == "Joe")
+                {
+                    joeNames.Add(name);
+                }
+            }
+            Console.WriteLine(String.Join("\n", joeNames));
+            Console.ReadLine();
+
+            List<string> NamesWithJoe = names.Where(x => x == "Joe").ToList();
+
+            Console.WriteLine(String.Join("\n", NamesWithJoe));
+            Console.ReadLine();
+
+            List<int> IDGreaterThanFive = IDs.Where(x => x > 5).ToList();
+
+            Console.WriteLine(String.Join("\n", IDGreaterThanFive));
+            Console.ReadLine();
+        }
+    }
+}
