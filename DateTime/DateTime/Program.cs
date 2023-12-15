6bb4b349a1564a267b6acb6c6587ce4f66fd8f0d
Tanner Vasquez <126614745+TVisi0n@users.noreply.github.com>
2023-12-14 19:00:17 -0800

    Practicing date and time

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime CurrentTime = DateTime.Now;
+
+            Console.WriteLine("Please enter a number");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            TimeSpan result = TimeSpan.FromHours(userInput);
+
+            DateTime answer = CurrentTime.Add(result);
+
+            Console.WriteLine("The current time plus {0} hours is: {1}", result, answer);
+
+            Console.ReadLine();
+        }
+    }
+}
